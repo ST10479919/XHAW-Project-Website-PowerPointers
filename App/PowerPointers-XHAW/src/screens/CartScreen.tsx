c13bfc3526eacdee93c26345cf4abaa9a4b1949d
Jaco <ST10479919@vcconnect.edu.za>
2025-10-15 10:34:54 +0200

    pref: Made small changes to style for text to pop out

--- App/PowerPointers-XHAW/src/screens/CartScreen.tsx
+++ App/PowerPointers-XHAW/src/screens/CartScreen.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import {
-  Text,
-  View,
-  Pressable,
-  FlatList,
-  Alert,
-  SectionList,
-  Button,
+    Text,
+    View,
+    Pressable,
+    FlatList,
+    Alert,
+    SectionList,
+    Button,
 } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -17,19 +17,19 @@ import { coursesData, CourseType, Course } from "../services/courses";
 type Props = NativeStackScreenProps<RootStackParamList, "Cart">;
 
 type CartItem = {
-  course: Course;
-  courseType: CourseType;
+    course: Course;
+    courseType: CourseType;
 };
 
-export default function CartScreen ({ navigation, route }: Props) {
+export default function CartScreen({ navigation, route }: Props) {
     const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
     const addToCart = (course: Course, courseType: CourseType) => {
         const isAddedItem = cartItems.find(item => item.course.title === course.title && item.courseType === courseType);
         if (!isAddedItem) {
-            setCartItems(prev => [...prev, {course, courseType}]);
+            setCartItems(prev => [...prev, { course, courseType }]);
             return true;
-        } 
+        }
         return false;
     };
 
@@ -43,7 +43,7 @@ export default function CartScreen ({ navigation, route }: Props) {
         const courseAmount = cartItems.length
         var discountRate = 0;
         const subTotal = cartItems.reduce((total, item) => total + item.course.price, 0)
-        
+
         if (courseAmount > 1) {
             switch (courseAmount) {
                 case 2:
@@ -70,25 +70,25 @@ export default function CartScreen ({ navigation, route }: Props) {
             finalTotal
         }
     };
-    
+
     const isCourseInCart = (courseTitle: string) => {
         return cartItems.some(item => item.course.title === courseTitle);
     };
 
 
-    const courseSections = [ {
-            title: "6-Month Courses",
-            data: coursesData["6-Months"],
-            type: "6-Months" as CourseType
-        },
-        {
-            title: "6-Week Courses",
-            data: coursesData["6-Weeks"],
-            type: "6-Weeks" as CourseType
-        }
+    const courseSections = [{
+        title: "6-Month Courses",
+        data: coursesData["6-Months"],
+        type: "6-Months" as CourseType
+    },
+    {
+        title: "6-Week Courses",
+        data: coursesData["6-Weeks"],
+        type: "6-Weeks" as CourseType
+    }
     ];
 
-    const renderCourseItems = ({ item, section }: { item: Course, section: any}) => (
+    const renderCourseItems = ({ item, section }: { item: Course, section: any }) => (
         <View style={styles.courseItem}>
             <View style={styles.cartItemInfo}>
                 <Text style={styles.courseTitle}>{item.title}</Text>
@@ -96,23 +96,23 @@ export default function CartScreen ({ navigation, route }: Props) {
                 <Text style={styles.coursePrice}>R{item.price}</Text>
             </View>
 
-        {isCourseInCart(item.title) ? (
-            <Pressable
-                style={[styles.button, {backgroundColor: "#cf3726ff"}]}
-                onPress={() => removeFromCart(item.title)}>
-                <Text style={{color:"#fff"}}>Remove</Text>
-            </Pressable>
-             ) : (
-            <Pressable
-                style={styles.button}
-                onPress={() => addToCart(item, section.type)}>
-                <Text style={{color:"#fff"}}>Add</Text>
-            </Pressable>
-        )}
+            {isCourseInCart(item.title) ? (
+                <Pressable
+                    style={[styles.button, { backgroundColor: "#cf3726ff" }]}
+                    onPress={() => removeFromCart(item.title)}>
+                    <Text style={{ color: "#fff" }}>Remove</Text>
+                </Pressable>
+            ) : (
+                <Pressable
+                    style={styles.button}
+                    onPress={() => addToCart(item, section.type)}>
+                    <Text style={{ color: "#fff" }}>Add</Text>
+                </Pressable>
+            )}
         </View>
     );
 
-    const renderSections = ({ section }: {section: any}) => (
+    const renderSections = ({ section }: { section: any }) => (
         <View style={styles.textSection}>
             <Text style={styles.sectionTitle}>{section.title}</Text>
         </View>
@@ -129,62 +129,58 @@ export default function CartScreen ({ navigation, route }: Props) {
         Alert.alert(
             "Checkout Complete",
             "Thank you for your purchase!",
-            [{ text: "OK", onPress: () => {
-                clearCart();
-                navigation.navigate("Home");
-            }}]
+            [{
+                text: "OK", onPress: () => {
+                    clearCart();
+                    navigation.navigate("Home");
+                }
+            }]
         )
     };
 
 
-    return(
+    return (
         <SafeAreaView style={styles.container}>
-                    <Pressable
-                    style={styles.button}
-                    onPress={() => navigation.goBack()}
-                    ><Text style={{color:"#fff"}}>Go back</Text>
-                    </Pressable>
             <Text style={styles.screenTitle}>Select Courses</Text>
-            <Text style={styles.courseCount}>{cartItems.length} courses added</Text>
-        
 
-        <SectionList 
-            sections={courseSections}
-            renderSectionHeader={renderSections}
-            renderItem={renderCourseItems}
-            keyExtractor={(item, index) => item.title + index}
-            showsVerticalScrollIndicator={true}
-        />
 
-        <View style={styles.textSection}>
-            <Text style={styles.sectionTitle}>Summary</Text>
-            <View style={styles.costSection}>
-                <View style={styles.costDisplay}>
-                    <Text style={styles.sectionContent}>Subtotal:</Text>
-                    <Text style={styles.sectionContent}>R {costs.subTotal}</Text>
-                </View>
-                <View style={styles.costDisplay}>
-                    <Text style={styles.sectionContent}>VAT cost:</Text>
-                    <Text style={styles.sectionContent}>R {costs.vatCost.toFixed(2)}</Text>
-                </View>
-                <View style={styles.costDisplay}>
-                    <Text style={styles.sectionContent}>Discount: {costs.discountRate*100}%</Text>
-                    <Text style={styles.sectionContent}>R {(costs.discountAmount).toFixed(2)}</Text>
-                </View>
-                <View style={styles.costDisplay}>
-                    <Text style={styles.sectionContent}>Final cost:</Text>
-                    <Text style={styles.sectionContent}>R {costs.finalTotal.toFixed(2)}</Text>
-                </View>
-                
+            <SectionList
+                sections={courseSections}
+                renderSectionHeader={renderSections}
+                renderItem={renderCourseItems}
+                keyExtractor={(item, index) => item.title + index}
+                showsVerticalScrollIndicator={true}
+            />
+
+            <View style={styles.textSection}>
+                <Text style={styles.sectionTitle}>Summary</Text>
+                <View style={styles.costSection}>
+                    <View style={styles.costDisplay}>
+                        <Text style={styles.sectionContent}>Subtotal:</Text>
+                        <Text style={styles.sectionContent}>R {costs.subTotal}</Text>
+                    </View>
+                    <View style={styles.costDisplay}>
+                        <Text style={styles.sectionContent}>VAT cost:</Text>
+                        <Text style={styles.sectionContent}>R {costs.vatCost.toFixed(2)}</Text>
+                    </View>
+                    <View style={styles.costDisplay}>
+                        <Text style={styles.sectionContent}>Discount: {costs.discountRate * 100}%</Text>
+                        <Text style={styles.sectionContent}>R {(costs.discountAmount).toFixed(2)}</Text>
+                    </View>
+                    <View style={styles.costDisplay}>
+                        <Text style={styles.sectionContent}>Final cost:</Text>
+                        <Text style={styles.sectionContent}>R {costs.finalTotal.toFixed(2)}</Text>
+                    </View>
+
 
                     <Pressable
-                    style={styles.button}
-                    onPress={doCheckout}
-                    ><Text style={{color:"#fff"}}>Confirm</Text></Pressable>
-                
-            </View>
-        </View>
+                        style={styles.button}
+                        onPress={doCheckout}
+                    ><Text style={{ color: "#fff" }}>Confirm</Text></Pressable>
 
-    </SafeAreaView>
+                </View>
+            </View>
+
+        </SafeAreaView>
     )
 }
